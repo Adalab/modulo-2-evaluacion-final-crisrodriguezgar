6c074e596d84c8a50cfc174bdf7fdf33e9ca448f
crisrodriguezgar <rodriguezgar.cris@gmail.com>
2023-09-28 19:21:42 +0200

    added query selectors

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
 console.log ('holii');
+
+/**** query selectors****/
+const inputSearch = document.querySelector('.js-input');
+const btnSearch = document.querySelector('.js-btn-search');
+
+const listSeries = document.querySelector('.js-container');
+const favsSeries = document.querySelector('.js-favs');
